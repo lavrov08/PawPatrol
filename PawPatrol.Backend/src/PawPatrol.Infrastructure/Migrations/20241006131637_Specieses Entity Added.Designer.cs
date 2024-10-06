0f9cf551a3784df12f783673d6495fd0da2a5069
lavrov08 <5452894@gmail.com>
2024-10-06 16:26:30 +0300

    feat: Migration SpeciesesEntityAdded added

--- PawPatrol.Backend/src/PawPatrol.Infrastructure/Migrations/20241006131637_Specieses Entity Added.Designer.cs
+++ PawPatrol.Backend/src/PawPatrol.Infrastructure/Migrations/20241006131637_Specieses Entity Added.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PawPatrol.Infrastructure;
@@ -12,9 +13,11 @@ using PawPatrol.Infrastructure;
 namespace PawPatrol.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241006131637_Specieses Entity added")]
+    partial class SpeciesesEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PawPatrol.Backend/src/PawPatrol.Infrastructure/Migrations/20241006131637_Specieses Entity Added.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PawPatrol.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SpeciesesEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "breed_value",
+                schema: "public",
+                table: "pets");
+
+            migrationBuilder.DropColumn(
+                name: "kind",
+                schema: "public",
+                table: "pets");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "kind_breed_id",
+                schema: "public",
+                table: "pets",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "kind_species_id",
+                schema: "public",
+                table: "pets",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "species",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_species", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "breeds",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    species_id = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_breeds", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_breeds_species_species_id",
+                        column: x => x.species_id,
+                        principalSchema: "public",
+                        principalTable: "species",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_breeds_species_id",
+                schema: "public",
+                table: "breeds",
+                column: "species_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "breeds",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "species",
+                schema: "public");
+
+            migrationBuilder.DropColumn(
+                name: "kind_breed_id",
+                schema: "public",
+                table: "pets");
+
+            migrationBuilder.DropColumn(
+                name: "kind_species_id",
+                schema: "public",
+                table: "pets");
+
+            migrationBuilder.AddColumn<string>(
+                name: "breed_value",
+                schema: "public",
+                table: "pets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "kind",
+                schema: "public",
+                table: "pets",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
